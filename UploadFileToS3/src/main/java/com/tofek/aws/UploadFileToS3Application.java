9ecde594158a7596d18c62ed25b2364bf0a718d3
tkhan10 <tofeequekhan87@gmail.com>
2025-04-19 11:19:02 -0500

    first commit.

---
+++ UploadFileToS3/src/main/java/com/tofek/aws/UploadFileToS3Application.java
@@ -0,0 +1,13 @@
+package com.tofek.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadFileToS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadFileToS3Application.class, args);
+	}
+
+}
---
+++ UploadFileToS3/src/test/java/com/tofek/aws/UploadFileToS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tofek.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadFileToS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
