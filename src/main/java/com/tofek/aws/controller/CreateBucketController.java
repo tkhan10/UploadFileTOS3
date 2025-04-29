b1d4cf8b8c095411c167a7b7f27460a21eee9038
tkhan10 <tofeequekhan87@gmail.com>
2025-04-29 06:23:57 -0500

    upload data to s3.

--- src/main/java/com/tofek/aws/controller/CreateBucketController.java
+++ src/main/java/com/tofek/aws/controller/CreateBucketController.java
@@ -2,10 +2,13 @@ package com.tofek.aws.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import software.amazon.awssdk.core.SdkRequest;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.core.sync.RequestBody;
+
+import java.io.File;
 
 
 @RestController
@@ -28,6 +31,15 @@ public class CreateBucketController {
 
     @GetMapping("addData")
     public void addObject(){
-        String bucketName = "tofek-bucket";
+        try {
+            String bucketName = "tofek-bucket";
+            String key = "I94_Saman_2.pdf";
+            File file = new File("/Users/tofekkhan/JavaProjectSpring/sourcefolder/I94_Saman_2.pdf");
+            PutObjectRequest request = PutObjectRequest.builder().bucket(bucketName).key(key).build();
+            s3Client.putObject(request, RequestBody.fromFile(file));
+            System.out.println("-------File uploaded successfully-------");
+        }catch (Exception e){
+            System.out.println("-------File upload failed--------");
+        }
     }
 }
